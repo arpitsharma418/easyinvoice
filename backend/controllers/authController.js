07ccb64431f6491fa1d90eef3f0bb05b0f333903
Arpit Sharma <as2501759@gmail.com>
2026-02-17 22:46:33 +0530

    Add frontend url in backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,8 +27,8 @@ export const registerUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      // secure: true,
-      // sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -79,7 +79,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
